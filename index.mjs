2e0e6145f419c607224d013ba2831145606b3a4a
mist-miguel-lara <123275349+mist-miguel-lara@users.noreply.github.com>
2026-04-09 16:59:26 -0700

    lab 7 initial code

---
+++ index.mjs
@@ -0,0 +1,70 @@
+import express from 'express';
+import mysql from 'mysql2/promise';
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+//for Express to get values using the POST method
+app.use(express.urlencoded({extended:true}));
+
+//setting up database connection pool, replace values in red
+const pool = mysql.createPool({
+    host: "sh4ob67ph9l80v61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "w9c7lwn8um1o99yj",
+    password: "u3rw8lbcasz2h307",
+    database: "pyn5h5u7iu857dd2",
+    connectionLimit: 10,
+    waitForConnections: true
+});
+//routes
+app.get('/', (req, res) => {
+   res.render('home.ejs')
+});
+
+//route to display the form to add a new author
+app.get('/addAuthor', (req, res) => {
+   res.render('addAuthor.ejs')
+});
+
+//route to save the author info into the database
+app.post('/addAuthor', async (req, res) => {
+
+    let firstName = req.body.firstName;
+    let lastName = req.body.lastName;
+    let dob = req.body.dob;
+    let bio = req.body.bio;
+
+    let sql = `INSERT INTO authors
+               (firstName, lastName, dob, biography)
+               VALUES
+               (?, ?, ?, ?)`;
+    let sqlParams = [firstName, lastName, dob, bio];
+
+    const [rows] = await pool.query(sql, sqlParams);
+
+   res.redirect('/');
+
+});
+
+//route to display the form to add a new quote
+app.get('/addQuote', (req, res) => {
+
+    //get list of authors
+    //get list of categories
+
+   res.render('addQuote.ejs')
+});
+
+
+app.get("/dbTest", async(req, res) => {
+   try {
+        const [rows] = await pool.query("SELECT CURDATE()");
+        res.send(rows);
+    } catch (err) {
+        console.error("Database error:", err);
+        res.status(500).send("Database error!");
+    }
+});//dbTest
+app.listen(3000, ()=>{
+    console.log("Express server running")
+})
